7c62440d64938df15746a4932a408de14a88afcd
divyanshgandhilm <120566424+divyanshgandhilm@users.noreply.github.com>
2023-09-25 18:53:16 +0530

    Adding customisation in GDS

--- lib/src/widgets/common/sheets/group_detail_bottom_sheet.dart
+++ lib/src/widgets/common/sheets/group_detail_bottom_sheet.dart
@@ -4,46 +4,56 @@ import 'package:likeminds_chat_fl/likeminds_chat_fl.dart';
 import 'package:likeminds_chat_ui_fl/likeminds_chat_ui_fl.dart';
 import 'package:likeminds_chat_ui_fl/src/utils/theme.dart';
 
-class LMGroupDetailBottomSheet extends StatefulWidget {
+class LMGroupDetailBottomSheet extends StatelessWidget {
+  // Required model variable
   final ChatRoom chatRoom;
+
+  //Customisability variables
+  final LMTextView? title;
+  final LMTextView? description;
+  final LMTextView? descriptionHeading;
+
+  final double? maxHeight;
+  final double? borderRadius;
+
+  final Color? backgroundColor;
+  final Color? shadowColor;
+  final Color? swipeChipColor;
+  final LinearGradient? overlayGradient;
+
   const LMGroupDetailBottomSheet({
     super.key,
     required this.chatRoom,
+    this.title,
+    this.description,
+    this.descriptionHeading,
+    this.maxHeight,
+    this.borderRadius,
+    this.backgroundColor,
+    this.shadowColor,
+    this.swipeChipColor,
+    this.overlayGradient,
   });
-
-  @override
-  State<LMGroupDetailBottomSheet> createState() =>
-      _LMGroupDetailBottomSheetState();
-}
-
-class _LMGroupDetailBottomSheetState extends State<LMGroupDetailBottomSheet> {
-  late ChatRoom chatRoom;
-
-  @override
-  void initState() {
-    chatRoom = widget.chatRoom;
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
-        height: 90.h,
+        height: maxHeight ?? 90.h,
         width: 100.w,
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: backgroundColor ?? Colors.white,
           boxShadow: [
             BoxShadow(
-              color: Colors.black.withOpacity(0.2),
+              color: shadowColor ?? Colors.black.withOpacity(0.2),
               offset: const Offset(0, -4),
               blurRadius: 8.0,
               spreadRadius: 4.0,
             )
           ],
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(8),
-            topRight: Radius.circular(8),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(borderRadius ?? 8),
+            topRight: Radius.circular(borderRadius ?? 8),
           ),
         ),
         child: Padding(
@@ -55,9 +65,9 @@ class _LMGroupDetailBottomSheetState extends State<LMGroupDetailBottomSheet> {
                 Container(
                   height: 0.6.h,
                   width: 10.w,
-                  decoration: const BoxDecoration(
-                    color: kGrey2Color,
-                    borderRadius: BorderRadius.all(
+                  decoration: BoxDecoration(
+                    color: swipeChipColor ?? kGrey2Color,
+                    borderRadius: const BorderRadius.all(
                       Radius.circular(4),
                     ),
                   ),
@@ -79,6 +89,9 @@ class _LMGroupDetailBottomSheetState extends State<LMGroupDetailBottomSheet> {
                             width: 100.w,
                             height: 36.h,
                             fit: BoxFit.cover,
+                            placeholder: (context, url) {
+                              return const LMMediaShimmer();
+                            },
                             imageUrl: chatRoom.chatroomImageUrl ??
                                 "https://placehold.co/600x400/000000/FFF?text=Chatroom",
                           )
@@ -88,18 +101,19 @@ class _LMGroupDetailBottomSheetState extends State<LMGroupDetailBottomSheet> {
                           ),
                     Container(
                       height: 36.h,
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Colors.black,
-                            Colors.transparent,
-                            Colors.transparent,
-                            Colors.black
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          stops: [0, 0, 0.6, 1],
-                        ),
+                      decoration: BoxDecoration(
+                        gradient: overlayGradient ??
+                            const LinearGradient(
+                              colors: [
+                                Colors.black,
+                                Colors.transparent,
+                                Colors.transparent,
+                                Colors.black
+                              ],
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              stops: [0, 0, 0.6, 1],
+                            ),
                       ),
                     ),
                     Positioned(
@@ -108,41 +122,46 @@ class _LMGroupDetailBottomSheetState extends State<LMGroupDetailBottomSheet> {
                         right: 18,
                         child: Padding(
                           padding: const EdgeInsets.all(4.0),
-                          child: LMTextView(
-                            text: chatRoom.header,
-                            overflow: TextOverflow.ellipsis,
-                            textStyle: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 28,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
+                          child: title ??
+                              LMTextView(
+                                text: chatRoom.header,
+                                overflow: TextOverflow.ellipsis,
+                                textStyle: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
                         ))
                   ],
                 ),
-                const SizedBox(height: 24),
+                const SizedBox(height: 12),
                 Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const LMTextView(
-                        text: "Description",
-                        textStyle: TextStyle(
-                          color: Colors.black,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
+                  padding: EdgeInsets.all(12.0),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const LMTextView(
+                          text: "Description",
+                          textStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 8),
-                      LMTextView(
-                        text: chatRoom.title,
-                        textStyle: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
+                        const SizedBox(height: 8),
+                        description ??
+                            LMTextView(
+                              text: chatRoom.title,
+                              textStyle: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                              ),
+                            ),
+                      ],
+                    ),
                   ),
                 )
               ],
