18fd49565b4523d762d6b2022dd84e3ec36c8014
Anuj Kumar <144224503+AnujLM@users.noreply.github.com>
2023-11-14 14:35:03 +0530

    add all models export to models.dart

--- lib/likeminds_chat_ui_fl.dart
+++ lib/likeminds_chat_ui_fl.dart
@@ -2,6 +2,4 @@ library likeminds_chat_ui_fl;
 
 export 'src/widgets/widgets.dart';
 export 'src/utils/utils.dart';
-export 'src/models/media_model.dart';
-export 'src/models/menu_item_model.dart';
-export 'src/models/tag_view_data_model.dart';
+export 'src/models/models.dart';
---
+++ lib/src/models/models.dart
@@ -0,0 +1,3 @@
+export 'media_model.dart';
+export 'menu_item_model.dart';
+export 'tag_view_data_model.dart';
